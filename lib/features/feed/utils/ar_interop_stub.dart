4e542caf9e4d9c6df1eeeca928c728eff99dc732
Sourav Barman <soubarman62@gmail.com>
2026-06-11 01:07:52 -0700

    Both side Camera

--- lib/features/feed/utils/ar_interop_stub.dart
+++ lib/features/feed/utils/ar_interop_stub.dart
@@ -1,4 +1,4 @@
-dynamic initializeARTracker(dynamic videoElement) => throw UnsupportedError('AR tracker is web-only');
+dynamic initializeARTracker(dynamic videoElement, String facingMode) => throw UnsupportedError('AR tracker is web-only');
 
 void setARFilter(String filterName) {}
 
